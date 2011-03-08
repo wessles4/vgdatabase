408fa4c58f9d42331f6c11d186dbe63f09bf59a6
lateofftheblock <bodonnel@mail.umw.edu>
2011-03-08 10:34:53 -0500

    my games page. no php in it yet

---
+++ mygames.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<style type="text/css">
+th {text-align: left;}
+th {border-bottom: 1px solid black;}
+
+</style>
+<Title>The Stack - My Games</title>
+<body bgcolor=black>
+
+<?PHP
+	include('header.php');
+?>
+<div id="page">
+	<div id="page-bgtop">
+		<div id="page-bgbtm">
+			<div id="content">
+				<div class="post">
+					<div class="post-bgtop">
+						<div class="post-bgbtm">
+							<div class="entry">
+							<h1 align=left>MY GAMES:</h1>
+							<form method="post">
+							
+							</form>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+	</div>
+</div>
+</body>
+</html>
+	
