e1247a9764bf4681f782c100b302a8f5d6cb59f6
jhouchen <jhouchen@umw.edu>
2011-04-06 16:39:42 -0400

    changes

--- home.php
+++ home.php
@@ -7,6 +7,7 @@ session_start();
 <body>
 <?php
 	include('header.php');
+	include('db_connect.php');
 ?>
 <div id="page">
 	<div id="page-bgtop">
@@ -29,8 +30,8 @@ echo "<p><font size=3 color=grey>Search for games based on title, platform, genr
 							<form method="post" action="search.php">
 							<p><font color=grey size=3>Query our database for videogame information. Search by game name.</p>
 							<br>
-							Search:</font><input style="font-size:default"  type="text" name="search" id="search" size=78%/><br><br>
-							<input type="submit" value="go" name="submit" />
+							Search:<br></font><input style="font-size:default"  type="text" name="search" id="search" size=78%/>
+							<input type="submit" value="Go" name="submit" />
 							</form>
 							</div>
 						</div>
@@ -60,6 +61,38 @@ echo "<p><font size=3 color=grey>Search for games based on title, platform, genr
 						<h2>LATEST GAMES</h2>
 						<ul>
 						<!--- INCLUDE A PHP FILE THAT BRINGS UP THE 10 LATEST GAMES ON THE LIST --->
+						<?php
+						$query = "SELECT name, platform FROM videogames ORDER BY game_id DESC LIMIT 5";
+						$result = mysqli_query($db, $query)
+									or die("Error Querying Database");
+						while($row = mysqli_fetch_array($result)){
+							echo "<li>";
+							echo $row['name'];
+							echo "    |    <b>";
+							echo $row['platform'];
+							echo "</b></li>";
+						}
+						?>
+						</ul>
+					</li>
+					<li>
+						<h2>TOP RATED</h2>
+						<ul>
+						<!--- Top rated games on the site --->
+						<?php
+						$query2 = "SELECT name, platform, ign_score FROM videogames ORDER BY ign_score DESC LIMIT 5";
+						$result2 = mysqli_query($db, $query2)
+									or die("Error Querying Database");
+						while($row = mysqli_fetch_array($result2)){
+							echo "<li>";
+							echo $row['name'];
+							echo "    |    ";
+							echo $row['platform'];
+							echo "    |    <b>";
+							echo $row['ign_score'];
+							echo "</b></li>";
+						}
+						?>
 						</ul>
 					</li>
 				</ul>
